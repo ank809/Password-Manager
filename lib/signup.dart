29520889b230813d4c664dff1d46ef03ce76b7df
ank809 <ankita51523@gmail.com>
2023-07-26 22:45:39 +0530

    fixed issue in signup page

--- lib/signup.dart
+++ lib/signup.dart
@@ -21,116 +21,106 @@ class _SignState extends State<Sign> {
         centerTitle: true,
         backgroundColor: Color.fromARGB(255, 233, 226, 226),
       ),
-      body: Container(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(height: 40.0),
-            Text(
-              'Create an Account with',
-              style: TextStyle(
-                fontSize: 20.0,
-              ),
-            ),
-            SizedBox(height: 30.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IconContainer(
-                  onPressed: () {
-                    // Handle Google login
-                  },
-                  imagePath: 'Asset/images/google3.png',
-                  iconSize: 75.0,
-                ),
-                SizedBox(width: 20.0),
-                IconContainer(
-                  onPressed: () {
-                    // Handle Facebook login
-                  },
-                  imagePath: 'Asset/images/facebook1.png',
-                  iconSize: 80.0,
-                ),
-                SizedBox(width: 20.0),
-                IconContainer(
-                  onPressed: () {
-                    // Handle Twitter login
-                  },
-                  imagePath: 'Asset/images/twitter2.png',
-                  iconSize: 100.0,
-                ),
-              ],
-            ),
-            SizedBox(height: 50.0),
-            Text(
-              '-- Or create your account with --',
-              style: TextStyle(
-                fontSize: 20.0,
-              ),
-            ),
-            SizedBox(height: 30.0),
-            Expanded(
-              flex: 5,
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    TextFormField(
-                      decoration: InputDecoration(
-                        focusedBorder: OutlineInputBorder(),
-                        fillColor: Colors.white,
-                        filled: true,
-                        hintText: 'Enter your first name',
-                      ),
-                    ),
-                    SizedBox(height: 30.0),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        focusedBorder: OutlineInputBorder(),
-                        fillColor: Colors.white,
-                        filled: true,
-                        hintText: 'Enter your last name',
-                      ),
-                    ),
-
-                    SizedBox(height: 30.0),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        focusedBorder: OutlineInputBorder(),
-                        fillColor: Colors.white,
-                        filled: true,
-                        hintText: 'Enter your email',
-                      ),
-                    ),
-
-                    SizedBox(height: 70.0),
-
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        ElevatedButton(
-                          onPressed: () {
-                            // Handle create account action
-                          },
-                          child: Text(
-                            'Create Account',
-                            style: TextStyle(
-                              color: Colors.green,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 22.0,
-                            ),
-                          ),
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Color.fromARGB(255, 233, 226, 226),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 40.0),
+              Text(
+                'Create an Account with',
+                style: TextStyle(
+                  fontSize: 20.0,
                 ),
               ),
-            ),
-          ],
+              SizedBox(height: 30.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconContainer(
+                    onPressed: () {
+                      // Handle Google login
+                    },
+                    imagePath: 'Asset/images/google3.png',
+                    iconSize: 75.0,
+                  ),
+                  SizedBox(width: 20.0),
+                  IconContainer(
+                    onPressed: () {
+                      // Handle Facebook login
+                    },
+                    imagePath: 'Asset/images/facebook1.png',
+                    iconSize: 80.0,
+                  ),
+                  SizedBox(width: 20.0),
+                  IconContainer(
+                    onPressed: () {
+                      // Handle Twitter login
+                    },
+                    imagePath: 'Asset/images/twitter2.png',
+                    iconSize: 100.0,
+                  ),
+                ],
+              ),
+              SizedBox(height: 50.0),
+              Text(
+                '-- Or create your account with --',
+                style: TextStyle(
+                  fontSize: 20.0,
+                ),
+              ),
+              SizedBox(height: 30.0),
+              TextFormField(
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(),
+                  fillColor: Colors.white,
+                  filled: true,
+                  hintText: 'Enter your first name',
+                ),
+              ),
+              SizedBox(height: 30.0),
+              TextFormField(
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(),
+                  fillColor: Colors.white,
+                  filled: true,
+                  hintText: 'Enter your last name',
+                ),
+              ),
+              SizedBox(height: 30.0),
+              TextFormField(
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(),
+                  fillColor: Colors.white,
+                  filled: true,
+                  hintText: 'Enter your email',
+                ),
+              ),
+              SizedBox(height: 70.0),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      // Handle create account action
+                    },
+                    child: Text(
+                      'Create Account',
+                      style: TextStyle(
+                        color: Colors.green,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 22.0,
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromARGB(255, 233, 226, 226),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
